7d408800f66fdd6e4dba823ba712a2dd0bba5b5c
LiJunYi <1040381245@qq.com>
2022-11-08 20:11:19 +0800

    添加校验

--- element-ui-lib2/components/from/form.vue
+++ element-ui-lib2/components/from/form.vue
@@ -3,8 +3,13 @@ import { renderSlot, renderScopedSlots } from "../../utils";
 import base from "../../mixins/base";
 export default {
   mixins: [base],
+  props: {
+    submit: Function,
+    reset: Function,
+  },
   render() {
     return this.$createElement("el-form", {
+      ref: "el-form",
       attrs: {
         ...this.bindDefaultAttrs(),
         ...this.$attrs,
@@ -14,7 +19,38 @@ export default {
       },
     });
   },
+  mounted() {
+    //
+  },
+  created() {
+    this.setSubmit();
+  },
+  beforeDestroy() {
+    //
+  },
   methods: {
+    setSubmit() {
+      if (!this.submit) return;
+      this.submit.onApply = () => {
+        this.$refs["el-form"].validate((valid) => {
+          if (valid) {
+            // console.log("success submit!!");
+            this.submit.lock = false;
+          } else {
+            // console.log("error submit!!");
+            this.submit.lock = true;
+            this.$emit("onErrorSubmit", this);
+            return false;
+          }
+        });
+      };
+    },
+    setReset() {
+      if (!this.reset) return;
+      this.submit.onApply = () => {
+        this.$refs["el-form"].resetFields();
+      };
+    },
     renderDefault() {
       return null;
     },
